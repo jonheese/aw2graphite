e3911067e1bf74a182c3549e349c32a8a37d0383
Jon Heese <github@jonheese.com>
2022-01-04 13:52:38 -0500

    Add proper thresholding. simplify config/alerts file with bind mounts

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,12 @@
 FROM python:3.8.1-alpine
 WORKDIR /usr/src/app
-COPY . /usr/src/app
+COPY requirements.txt /usr/src/app/requirements.txt
 RUN set -eux \
-    && apk add --no-cache --virtual .build-deps build-base \
-        libressl-dev libffi-dev gcc musl-dev python3-dev \
     && pip install --upgrade pip setuptools wheel \
     && rm -rf /root/.cache/pip \
     && pip install -r /usr/src/app/requirements.txt \
     && rm -rf /root/.cache/pip
+COPY aw2graphite-rt.py /usr/src/app/aw2graphite-rt.py
 ENV PYTHONDONTWRITEBYTECODE 1
 ENV PYTHONUNBUFFERED 1
 CMD ["python3", "aw2graphite-rt.py"]
--- aw2graphite-rt.py
+++ aw2graphite-rt.py
@@ -15,7 +15,7 @@ class Aw2Graphite:
 
     def __init__(self):
         self._log = logging.getLogger("aw2graphite-rt")
-        self._log.setLevel(logging.DEBUG)
+        self._log.setLevel(logging.INFO)
         ch = logging.StreamHandler()
         ch.setLevel(logging.DEBUG)
         formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
@@ -25,6 +25,9 @@ class Aw2Graphite:
         self._devices = []
         with open('./config.json', 'r') as f:
             config = json.load(f)
+            loglevel = config.get('LOGLEVEL')
+            if loglevel:
+                self._log.setLevel(loglevel)
             self.api_key = config.get('AW_API_KEY')
             self.app_key = config.get('AW_APPLICATION_KEY')
             self.carbon_server = config.get('CARBON_SERVER')
@@ -34,11 +37,14 @@ class Aw2Graphite:
             self.alert_from = config.get('ALERT_FROM')
             self.alert_to = config.get('ALERT_TO')
             self.alert_state_file = config.get('ALERT_STATE_FILE')
+            self.alert_thresholds = config.get('ALERT_THRESHOLDS')
 
         if 'AW_API_KEY' not in config.keys() or 'AW_APPLICATION_KEY' not in config.keys():
             raise RuntimeError(
                 'API key and applcation key must be specified in config.json'
             )
+        self.alerts = {}
+        self.load_alerts()
         self.__websocket = Websocket(self.app_key, self.api_key)
 
         self.__websocket.on_connect(self.connect)
@@ -71,6 +77,68 @@ class Aw2Graphite:
             else:
                 self._log.info("Not adding {mac} to my device list because it's already there")
 
+    def load_alerts(self):
+        try:
+            with open(self.alert_state_file, 'r') as f:
+                self._log.info(f"Loading alerts from {self.alert_state_file}")
+                self.alerts = json.loads(f.read())
+        except FileNotFoundError as e:
+            self.alerts = {}
+            self.save_alerts()
+
+    def save_alerts(self):
+        with open(self.alert_state_file, 'w') as f:
+            self._log.debug(f"Writing alerts to {self.alert_state_file}:")
+            self._log.debug(json.dumps(self.alerts, indent=2))
+            f.write(json.dumps(self.alerts, indent=2))
+
+    def update_alert(self, is_alerting, metric_name, alert_msg=None):
+        was_alerting = self.alerts.get(metric_name)
+        if was_alerting:
+            if not is_alerting:
+                self.send_alert_email(
+                    subject=f"RECOVERY: {metric_name}",
+                    message=alert_msg,
+                )
+        elif is_alerting:
+            self.send_alert_email(
+                subject=f"PROBLEM: {metric_name}",
+                message=alert_msg,
+            )
+        self.alerts[metric_name] = is_alerting
+
+    def send_alert_email(self, subject, message):
+        with smtplib.SMTP(self.smtp_server, self.smtp_port) as server:
+            server.sendmail(
+                self.alert_from,
+                self.alert_to,
+                f'Subject: {subject}\n\n{message}',
+            )
+
+    def check_if_alerting(self, metric_name, value):
+        threshold = None
+        is_alerting = False
+        if self.alert_thresholds and metric_name in self.alert_thresholds.keys():
+            operator = self.alert_thresholds[metric_name].get('operator')
+            threshold = self.alert_thresholds[metric_name].get('threshold')
+            if operator == 'gt':
+                is_alerting = value > threshold
+            elif operator == 'ge':
+                is_alerting = value >= threshold
+            elif operator == 'lt':
+                is_alerting = value < threshold
+            elif operator == 'le':
+                is_alerting = value <= threshold
+            elif operator == 'eq':
+                is_alerting = value == threshold
+            elif operator == 'ne':
+                is_alerting = value != threshold
+        self.update_alert(
+            is_alerting=is_alerting,
+            metric_name=metric_name,
+            alert_msg=f"Weather metric {metric_name} is at value {value}, configured threshold is {threshold}",
+        )
+
     def data(self, message):
         mac = message.get('macAddress')
         if mac not in self._devices:
@@ -87,50 +155,19 @@ class Aw2Graphite:
                 try:
                     alert = False
                     value = message.get(metric_name)
-                    if metric_name == 'dateutc':
-                        continue
-                    elif metric_name == 'battout' and value != 1:
-                        alert = f"Outside weather station battery needs replacing (battout = {value})"
-                    elif metric_name == 'batt_co2' and value != 1:
-                        alert = f"Inside weather station battery needs replacing (batt_co2 = {value})"
-                    if isinstance(value, (int, float)):
+                    if metric_name != 'dateutc' and isinstance(value, (int, float)):
                         s = f'{base_metric}.{metric_name} {value} {timestamp}\n'
-                        #self._log.info("Sending metric:")
-                        #self._log.info(s)
+                        #self._log.debug("Sending metric:")
+                        #self._log.debug(s)
                         sock.send(s.encode())
-                    if alert:
-                        found_alert = True
-                        already_alerted = False
-                        try:
-                            with open(self.alert_state_file) as f:
-                                if alert == f.readline():
-                                    already_alerted = True
-                        except Exception as e:
-                            pass
-                        if already_alerted:
-                            self._log.info(f"Already alerted for alert {alert} so not sending email")
-                        else:
-                            self._log.info(f"Sending alert email to {self.alert_to} with content: {alert}")
-                            with smtplib.SMTP(self.smtp_server, self.smtp_port) as server:
-                                server.sendmail(
-                                    self.alert_from,
-                                    self.alert_to,
-                                    f'Subject: {alert}\n\n{alert}'
-                                )
-                            with open(self.alert_state_file, 'w') as f:
-                                self._log.info(f"Writing alert to {self.alert_state_file}: ({alert})")
-                                f.write(alert)
+                        self.check_if_alerting(metric_name, value)
                 except Exception as e:
                     self._log.exception(e)
-            if not found_alert:
-                with open(self.alert_state_file, 'w') as f:
-                    self._log.info(f"Blanking {self.alert_state_file}")
-                    f.write('')
         except Exception as e:
             self._log.exception(e)
         finally:
+            self.save_alerts()
             sock.close()
-
 
 if __name__ == '__main__':
     aw2graphite = Aw2Graphite()
